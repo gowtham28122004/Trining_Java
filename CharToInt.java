a2c1a9cc08898783903d48d18491b13155f6d545
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2024-08-28 15:20:57 +0530

    Create CharToInt.java

---
+++ CharToInt.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class CharToInt {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        String name = s.nextLine();
+        
+        System.out.println("Upper Character : ");
+        for(int i = 0;i < name.length();i++) {
+            System.out.println((int) name.toUpperCase().charAt(i));
+        }
+        
+        System.out.println("Lower Character : ");
+        for(int i = 0;i < name.length();i++) {
+            System.out.println((int) name.toLowerCase().charAt(i));
+        }
+        
+    }
+}
