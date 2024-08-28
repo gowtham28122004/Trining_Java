278e080bba185b084ea048550c38821ebab2567b
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2024-08-28 15:30:09 +0530

    convert name to ASCII value.

--- CharToInt.java
+++ CharToInt.java
@@ -7,11 +7,18 @@ class CharToInt {
         
         String name = s.nextLine();
         
+        System.out.println("Position : ");
+        for(int i = 0;i < name.length();i++) {
+            System.out.println((int) (name.toUpperCase().charAt(i) - 'A')+1);
+        }
+        System.out.println();
+        
         System.out.println("Upper Character : ");
         for(int i = 0;i < name.length();i++) {
             System.out.println((int) name.toUpperCase().charAt(i));
         }
-        
+        System.out.println();
+
         System.out.println("Lower Character : ");
         for(int i = 0;i < name.length();i++) {
             System.out.println((int) name.toLowerCase().charAt(i));
