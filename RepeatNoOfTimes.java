b6f71fa85645544afda31990247a7dde173733cd
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2024-09-10 16:05:32 +0530

    Update RepeatNoOfTimes.java

--- RepeatNoOfTimes.java
+++ RepeatNoOfTimes.java
@@ -1,28 +1,31 @@
 import java.util.*;
 
-public class RepeatNoOfTimes
-{
-	public static void main(String[] args) {
-	    
-		Scanner s = new Scanner(System.in);
-		char[] piece = s.nextLine().toCharArray();
-		
-		for(int i = 0;i < piece.length;)
-		{
-		    int f = i;
-		    i++;
-		    int t = 0;
-		    
-		    while(i < piece.length && (piece[i] >= '0' && piece[i] <= '9')) {
-		        t *= 10;
-		        t += piece[i] - '0';
-		        i++;
-		    }
-		    
-		    while(t > 0) {
-		        System.out.print(piece[f]);
-		        t--;
-		    }
-		}
-	}
+public class RepeatNoOfTimes {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        s.close();
+        char[] piece = input.toCharArray();
+        
+        for (int i = 0; i < piece.length;) {
+            StringBuilder sb = new StringBuilder();
+            while (i < piece.length && ((piece[i] >= 'a' && piece[i] <= 'z') || (piece[i] >= 'A' && piece[i] <= 'Z'))) {
+                sb.append(piece[i]);
+                i++;
+            }
+            
+            if (i >= piece.length) break;
+            
+            int t = 0;
+            while (i < piece.length && (piece[i] >= '0' && piece[i] <= '9')) {
+                t = t * 10 + (piece[i] - '0');
+                i++;
+            }
+            
+            while (t > 0) {
+                System.out.print(sb);
+                t--;
+            }
+        }
+    }
 }
